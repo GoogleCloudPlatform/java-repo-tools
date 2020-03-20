2101996ac7a47db9e1a8997264cd09e7f8006ddc
Les Vogel <lesv@google.com>
2020-03-20 12:34:33 -0700

    allow newlines in imports

--- test/src/test/java/com/google/cloud/samples/test/AppTest.java
+++ test/src/test/java/com/google/cloud/samples/test/AppTest.java
@@ -20,6 +20,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
